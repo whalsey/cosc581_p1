53e0ebbcff921801aea708978a1c01511fe2d03f
whalsey <whalsey@users.noreply.github.com>
2016-03-03 12:12:30 -0500

    Parse out extraneous chars and check for palindrom

--- hw7.cpp
+++ hw7.cpp
@@ -10,6 +10,7 @@
 #include <cstring>
 #include <fstream>
 #include <streambuf>
+#include <algorithm>
 
 #define usage 	"\nUsage: ./<executable> <filename.txt>\n"
 #define VERBOSE true
@@ -17,6 +18,8 @@
 using namespace std;
 
 string readInText(char *);
+bool testString(string);
+void eliminateExtraCharacters(string);
 
 int main (int argc, char *argv[]) {
 	if (argc != 2) {
@@ -25,6 +28,35 @@ int main (int argc, char *argv[]) {
 	}
 
 	string text = readInText(argv[1]);
+	if (VERBOSE) cerr << "\nmain: The input string is " << text << "\n";
+
+	text.erase(remove(text.begin(), text.end(), ' '), text.end());
+	text.erase(remove(text.begin(), text.end(), '\t'), text.end());
+	text.erase(remove(text.begin(), text.end(), '\n'), text.end());
+	text.erase(remove(text.begin(), text.end(), ','), text.end());
+	text.erase(remove(text.begin(), text.end(), '.'), text.end());
+	text.erase(remove(text.begin(), text.end(), ';'), text.end());
+	text.erase(remove(text.begin(), text.end(), '!'), text.end());
+	text.erase(remove(text.begin(), text.end(), '?'), text.end());
+	text.erase(remove(text.begin(), text.end(), '"'), text.end());
+	text.erase(remove(text.begin(), text.end(), '\''), text.end());
+	text.erase(remove(text.begin(), text.end(), '-'), text.end());
+	text.erase(remove(text.begin(), text.end(), '0'), text.end());
+	text.erase(remove(text.begin(), text.end(), '1'), text.end());
+	text.erase(remove(text.begin(), text.end(), '2'), text.end());
+	text.erase(remove(text.begin(), text.end(), '3'), text.end());
+	text.erase(remove(text.begin(), text.end(), '4'), text.end());
+	text.erase(remove(text.begin(), text.end(), '5'), text.end());
+	text.erase(remove(text.begin(), text.end(), '6'), text.end());
+	text.erase(remove(text.begin(), text.end(), '7'), text.end());
+	text.erase(remove(text.begin(), text.end(), '8'), text.end());
+	text.erase(remove(text.begin(), text.end(), '9'), text.end());
+	if (VERBOSE) cerr << "\nmain: The string sans spaces is " << text << "\n";
+	
+	bool test = testString(text);
+	if (test) {
+		cout << "\nmain: The longest string is " << text << "with size " << text.size() << "\n";
+	}
 
 	return 0;
 }
@@ -32,23 +64,35 @@ int main (int argc, char *argv[]) {
 string readInText(char *Fname) {
 	if (VERBOSE) cout << "\nreadInText: Entered function\n";
 	
-	ifstream t(Fname);
-	string text;
-	t.seekg(0, ios::end);
-	text.reserve(t.tellg());
-	t.seekg(0, ios::beg);
-	
-	FILE *f;
-
 	int size;
+	string text;
+	
+	ifstream t(Fname);
+	t.seekg(0, ios::end);
+	size = t.tellg();
+	t.seekg(0, ios::beg);
 
-	f = fopen(Fname, "r");
+	text.reserve(size);
 
-	fseek(f, (long)0, SEEK_END);
-	size = (int)ftell(f);
-	fseek(f, (long)0, SEEK_SET);
+	text.assign((istreambuf_iterator<char>(t)), istreambuf_iterator<char>());
 
 	if (VERBOSE) cerr << "\nreadInText: The size of the file is " << size << " bytes\n";
 
 	return text;
 }
+
+bool testString(string text) {
+	int size = text.size();
+	for (int i=0; i<size/2; i++) {
+		if (text[i] != text[size-2-i])
+			return false;
+	}
+
+	return true;
+}
+
+void eliminateExtraCharacters(string) {
+
+}
+
+
