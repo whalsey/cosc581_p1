85fa67be709d9f912c88bad7840a09aa76cfa7f8
whalsey <whalsey@users.noreply.github.com>
2016-03-07 19:33:21 -0500

    Added random string generator

---
+++ randString.c
@@ -0,0 +1,28 @@
+/*
+ * randString.c written by William Halsey
+ * whalsey@vols.utk.edu
+ *
+ * Monday 7 March 2016
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define usage "USAGE: ./{randString Executable} {Number of characters in random string} > {output file}.txt\n"
+
+int main (int argc, char *argv[]) {
+
+	if (argc != 2) {
+		fprintf(stderr, usage);
+		exit(-1);
+	}
+
+	int num = atoi(argv[1]);
+
+	for (int i=0; i<num; i++) {
+		fprintf(stdout, "%c", (char)(rand()%26+97));
+	}
+	fprintf(stdout, "\n");
+
+	return 0;
+}
